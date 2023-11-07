81eeb35cca862da6889e429f3842f6bc9912898f
marcinx <mail@marcinx.com>
2023-11-08 00:50:51 +0300

    Whalestack

--- src/lib/sep6_client/config.rb
+++ src/lib/sep6_client/config.rb
@@ -1,9 +1,9 @@
 module Sep6Client
 
-  CLIENT_VERSION = '1.0.0'
+  CLIENT_VERSION = '2.0.0'
 
   CLIENT_NAME = 'sep6_client'
 
-  TRANSFER_SERVER = 'sep6.coinqvest.com'
+  TRANSFER_SERVER = 'sep6.whalestack.com'
 
 end
